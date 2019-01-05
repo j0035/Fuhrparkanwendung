1ef15c64a2df8600b54d051240d47d1df9a57b00
j0035 <karsten.helmut@gmail.com>
2019-01-05 16:01:20 +0100

    2019-01-05 f

---
+++ Fuhrparkanwendung/MenueItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkanwendung
+{
+    class MenueItem
+    {
+        string Name;
+        Func<Boolean> executableFunction;
+
+        public MenueItem(string Name, Func<Boolean> execute)
+        {
+            this.Name = Name;
+            executeMenueFunction(execute);
+        }
+
+        public void executeMenueFunction(Func<Boolean> execute)
+        {
+            executableFunction = execute;
+        }
+        
+        public void execute()
+        {
+            executableFunction();
+        }
+    }
+}
---
+++ Fuhrparkanwendung/menue.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Menue
+    {
+        ConsoleKeyInfo cki;
+        int currentItem;
+        string[] menueText = {};
+        int menueLength;
+        Boolean exit, playGame = false;
+
+        public Menue(string[] menueText)
+        {
+            this.menueText = menueText;
+            this.menueLength = this.menueText.Length;
+            currentItem = 0;
+        }
+
+        public void runMenue()
+        {
+            WriteMenueText();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Selection();
+                    menueIterator(cki);
+                    if (exit)
+                    {
+                        break;
+                    }
+                    else if (playGame)
+                    {
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        WriteMenueText();
+                    }
+                }
+            }
+        }
+
+        public void Selection()
+        {
+            cki = Console.ReadKey();
+        }
+
+        private void WriteMenueText()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                WriteSingleMenueItems(i);
+            }
+        }
+
+        private void WriteSingleMenueItems(int iterator)
+        {
+            if (iterator == currentItem)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+            }
+
+            Console.WriteLine(menueText[iterator]);
+
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        private void menueIterator(ConsoleKeyInfo cki)
+        {
+
+            if (cki.Key == ConsoleKey.DownArrow)
+            {
+                currentItem++;
+                checkForRimCase(1);
+            }
+            if (cki.Key == ConsoleKey.UpArrow)
+            {
+                currentItem--;
+                checkForRimCase(0);
+            }
+            if (cki.Key == ConsoleKey.Enter && currentItem == 3)
+            {
+                exit = true;
+            }
+            if (cki.Key == ConsoleKey.Enter && currentItem == 0)
+            {
+                playGame = true;
+            }
+        }
+
+        private void checkForRimCase(int TopBottom)
+        {
+            if (TopBottom == 0 && CompairTwoItems(0, currentItem))
+            {
+                currentItem = this.menueLength - 1;
+            }
+            if (TopBottom == 1 && CompairTwoItems(currentItem, this.menueLength - 1))
+            {
+                currentItem = 0;
+            }
+        }
+
+        private Boolean CompairTwoItems(int firstItem, int secondItem)
+        {
+            return firstItem > secondItem;
+        }
+    }
+}
