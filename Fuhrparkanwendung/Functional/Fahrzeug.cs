87e5e2156feabed567c041d1f51723dc4942d424
j0035 <karsten.helmut@gmail.com>
2019-01-14 20:33:26 +0100

    2019-01-14 f

---
+++ Fuhrparkanwendung/Functional/Fahrzeug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkanwendung.Functional
+{
+    abstract class Fahrzeug
+    {
+        public string Hersteller { get; set; }
+        public string Modell { get; set; }
+        public string Kennzeichen { get; set; }
+        public DateTime Erstzulassung { get; set; }
+        public float Anschaffungspreis {get; set; }
+
+        public Fahrzeug(string Hersteller, string Modell, string Kennzeichen, DateTime Erstzulassung, float Anschaffungspreis)
+        {
+            this.Hersteller = Hersteller;
+            this.Modell = Modell;
+            this.Kennzeichen = Kennzeichen;
+            this.Erstzulassung = Erstzulassung;
+            this.Anschaffungspreis = Anschaffungspreis;
+
+        }
+    }
+}
