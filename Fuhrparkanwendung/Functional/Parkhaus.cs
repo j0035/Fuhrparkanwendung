af8bd07e9b65569f8b62be03f47061c0a0263d60
j0035 <karsten.helmut@gmail.com>
2019-01-16 18:58:58 +0100

    2019-01-16 f

---
+++ Fuhrparkanwendung/Functional/Parkhaus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkanwendung.Functional
+{
+    abstract class Parkhaus
+    {
+        string Parkhausname { get; set; }
+        int AnzahlParkplaetze { get; set; }
+        Parkplatz[] Plaetze { get; set; }
+        string Ort { get; set; } 
+        string Adresse { get; set; }
+        string Plz { get; set; }
+        int _anzMPlatz;
+        int _anzAPlatz { get; set; }
+        int _anzLPlatz { get; set; }
+
+        public Parkhaus(string Parkhausname, int AnzahlParkplaetze, Parkplatz[] Plaetze, string Ort, string Adresse, string Plz)
+        {
+            this.Parkhausname = Parkhausname;
+            this.AnzahlParkplaetze = AnzahlParkplaetze;
+            this.Plaetze = Plaetze;
+            this.Ort = Ort;
+            this.Adresse = Adresse;
+            this.Plz = Plz;
+        }
+
+        public int anzMPlatz
+        {
+            get { return _anzMPlatz; }
+            set
+            {
+                int temp = 0;
+                foreach (Parkplatz Platz in Plaetze) {
+                    if (Platz.Typ == 2)
+                    {
+                        temp++;
+                    }
+                }
+
+                _anzMPlatz = temp;
+            }
+        }
+    }
+}
---
+++ Fuhrparkanwendung/Functional/Parkplatz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkanwendung.Functional
+{
+    abstract class Parkplatz
+    {
+        public string Id { get; set; }
+        public bool Besetzt { get; set; }
+        public int Typ { get; set; }
+        public string _Kennzeichen { get; set; }
+        
+        public Parkplatz(string Id, int Typ, bool Besetzt = false)
+        {
+            this.Id = Id;
+            this.Typ = Typ;
+            this.Besetzt = Besetzt;
+        }
+
+        public string Kennzeichen
+        {
+            get { return _Kennzeichen; }
+            set 
+            {
+                if (Besetzt)
+                {
+                    throw new InvalidOperationException("Parkplatz ist besetzt");
+                }
+                else
+                {
+                    _Kennzeichen = value;
+                }
+            }
+        }
+        
+    }
+}
