ee42d78978ca9a94ddceb85ddb5ef59066f4366e
j0035 <karsten.helmut@gmail.com>
2019-01-16 20:40:33 +0100

    2019-01-16 s

---
+++ Fuhrparkanwendung/Data/WriteReadPkw.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fuhrparkanwendung.Functional;
+
+namespace Fuhrparkanwendung.Data
+{
+    class WriteReadPkw
+    {
+        string filePath;
+        Pkw[] AllePkw;
+
+        public WriteReadPkw()
+        {
+
+        }
+    }
+}
--- Fuhrparkanwendung/Program.cs
+++ Fuhrparkanwendung/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fuhrparkanwendung.Data;
 
 namespace Fuhrparkanwendung
 {
@@ -10,20 +11,26 @@ namespace Fuhrparkanwendung
     {
         static void Main(string[] args)
         {
-            test neu = new test();
-            Action test = () => neu.function();
-            MenueItem foo = new MenueItem("Test", test);
-            foo.execute();
-            Console.ReadKey();
+            //test neu = new test();
+            //Action test = () => neu.function();
+            //MenueItem foo = new MenueItem("Test", test);
+            //foo.execute();
+            //Console.ReadKey();
+
+            FileHandler file = new FileHandler();
+
+            string[] t_set = { "Hallo", "Welt", "ich", "bin", "der", "Helmut" };
+
+            file.Save("./Data/Test/test.csv", t_set, true, true);
         }
     }
 
-    internal class test
-    {
-        public void function()
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
+    //internal class test
+    //{
+    //    public void function()
+    //    {
+    //        Console.WriteLine("Hello World");
+    //    }
+    //}
     
 }
